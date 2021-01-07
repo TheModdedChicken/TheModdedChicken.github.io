a9fb560ea618b2ef3f051edcce7d1a4645d1d7d4
TheModdedChicken <loganshaw1744@gmail.com>
2021-01-07 15:40:15 +0800

    test

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
 /* Home page events */
 
-function loadLine() {
-    var profileCanvasObject = document.getElementById('profile-space');
-    var profileCanvas = profileCanvasObject.getContext("2d");
-
-    profileCanvas.strokeStyle = "#ffffff";
-    profileCanvas.beginPath();
-    profileCanvas.moveTo(20, 20);
-    profileCanvas.lineTo(20, 100);
-    profileCanvas.lineTo(70, 100);
-    profileCanvas.stroke();
-}
-
 function arrowHoverOffEvent(name) {
     var arrow = document.getElementById(name);
 
